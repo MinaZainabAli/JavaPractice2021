00a5529e6df7b7a75aeda16d35c3dc744ae36676
Mina Zainab Ali <minazainab@hotmail.com>
2021-09-27 16:38:19 -0400

    TypeCasting

---
+++ TypeCasting.java
@@ -0,0 +1,20 @@
+package javapractice2021;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		
+		/*  int a = 9, b = 4;
+			double c = (double)a / b;
+			System.out.println(c);	
+		*/
+		
+		int a = 2, b = 9;
+		a = b++;
+		
+		System.out.println(a);	
+		System.out.println(b);	
+		System.out.println(a);	
+	}
+
+}

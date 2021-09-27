43b793b240469e6f146377abeadbd9c16f16bfde
Mina Zainab Ali <minazainab@hotmail.com>
2021-09-27 16:36:07 -0400

    ArithematicOperations

---
+++ ArithematicOperations.java
@@ -0,0 +1,14 @@
+
+public class ArithematicOperations {
+
+	public static void main(String[] args) {
+		int a,b,c,d;
+		a = - 5 + 8 * 6;
+		b = (55+9)%9;
+		c = 20+-3*5/8;
+		d = 5+15/3*2-8%3;
+		System.out.println("Result: \n"+ a +"\n"+ b +"\n"+ c +"\n"+ d);
+
+	}
+
+}

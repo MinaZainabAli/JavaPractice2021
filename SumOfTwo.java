6390f1ed2fc7568b185d999192f060b6b4362d1e
Mina Zainab Ali <minazainab@hotmail.com>
2021-09-27 16:38:01 -0400

    SumOfTwo

---
+++ SumOfTwo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SumOfTwo {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner (System.in);
+		
+		System.out.print("Type a: ");
+		int a = input.nextInt();
+		
+		System.out.print("Type b: ");
+		int b = input.nextInt();
+		
+		System.out.println("Sum is equal to " + (a+b));
+		
+
+	}
+
+}
